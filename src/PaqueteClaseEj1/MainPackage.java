2a2e3866547ec51ba0b642bb94f0ad8df8f0d4fe
danisteb90 <esteban.alban9003@gmail.com>
2022-07-07 20:06:59 -0500

    first commit

---
+++ src/PaqueteClaseEj1/MainPackage.java
@@ -0,0 +1,25 @@
+package PaqueteClaseEj1;
+
+public class MainPackage {
+
+    public static void main(String[] args) {
+
+        int num1 = 25;
+        float num2 = 20.55f;
+        long num3 = 1245;
+        double num4 = 56.54d;
+        char letra = 'A';
+        String nombre = "Esteban";
+        boolean bool = true;
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(letra);
+        System.out.println(nombre);
+        System.out.println(bool);
+
+        }
+
+}
